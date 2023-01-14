500cf9c9d8a211361f20d86c23aad1664941015a
bamboo-wood <cantate.1009.0417@gmail.com>
2023-01-14 17:51:11 +0900

    change the cell background color

--- UIKit-Tutorial/UIKit-Tutorial/ListViewController/ReminderListViewController+DataSource.swift
+++ UIKit-Tutorial/UIKit-Tutorial/ListViewController/ReminderListViewController+DataSource.swift
@@ -13,11 +13,16 @@ extension ReminderListViewController {
     
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
         let reminder = Reminder.sampleData[indexPath.item]
+        
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(red: 1.0, green: 0.8, blue: 0.9, alpha: 1.0)
+        cell.backgroundConfiguration = backgroundConfiguration
     }
 }
 
