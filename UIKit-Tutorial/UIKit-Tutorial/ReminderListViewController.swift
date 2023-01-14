1d3c8dbc3847ffcdbbb3734c6fbefe19fda2b938
bamboo-wood <cantate.1009.0417@gmail.com>
2023-01-14 16:53:29 +0900

    set the value to collectionViewLayout

--- UIKit-Tutorial/UIKit-Tutorial/ReminderListViewController.swift
+++ UIKit-Tutorial/UIKit-Tutorial/ReminderListViewController.swift
@@ -11,7 +11,8 @@ class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.collectionViewLayout = listLayout()
     }
     
     private func listLayout() -> UICollectionViewCompositionalLayout {
