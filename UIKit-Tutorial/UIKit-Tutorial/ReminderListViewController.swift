9e83bd85470dc9f12f396a5f6c7f0283df46b938
bamboo-wood <cantate.1009.0417@gmail.com>
2023-01-14 17:01:45 +0900

    configure the data source

--- UIKit-Tutorial/UIKit-Tutorial/ReminderListViewController.swift
+++ UIKit-Tutorial/UIKit-Tutorial/ReminderListViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.collectionViewLayout = listLayout()
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell,
+                                                                    indexPath: IndexPath,
+                                                                    itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
     }
     
     private func listLayout() -> UICollectionViewCompositionalLayout {
