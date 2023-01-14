685101caef36024de11e4407c3c85e2823d28a78
bamboo-wood <cantate.1009.0417@gmail.com>
2023-01-14 16:27:59 +0900

    create Xcode project

---
+++ UIKit-Tutorial/UIKit-Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Tutorial
+//
+//  Created by 佐々木一樹 on 2023/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
