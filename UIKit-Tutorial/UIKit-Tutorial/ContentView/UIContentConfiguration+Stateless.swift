8f141fc6545bea304ac1dd4be791c927a124ecbd
bamboo-wood <cantate.1009.0417@gmail.com>
2023-01-15 15:10:00 +0900

    conform to the content view protocol

---
+++ UIKit-Tutorial/UIKit-Tutorial/ContentView/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  UIKit-Tutorial
+//
+//  Created by 佐々木一樹 on 2023/01/15.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
